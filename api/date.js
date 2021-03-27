db6bbcf32b41fe4770152ddaee5bef7d2cdfc3ad
HR.Coder <50870450+Hamidrdeveloper@users.noreply.github.com>
2021-03-28 00:51:23 +0430

    Update date.js

--- api/date.js
+++ api/date.js
@@ -1,4 +1,4 @@
 module.exports = (req, res) => {
   const date = new Date().toString();
-  res.status(200).send(date);
+  res.status(200).send('http://dlrapfarsi.ir/Mp3/1399/Bahram/Bahram%20-%20Beshno.mp3');
 };
