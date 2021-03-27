9af65892bc8c535c8c7ec2036a4aecfa87d3c913
HR.Coder <50870450+Hamidrdeveloper@users.noreply.github.com>
2021-03-28 02:11:41 +0430

    Update date.js

--- api/date.js
+++ api/date.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
-  var date = {[{nid: 5,
+  var date = [{nid: 5,
       cover: 'https://myrapfarsi.com/wp-content/uploads/2021/03/Bahram%20-%20Beshno.jpg',
       title: "Beshno ",
       path: 'http://dlrapfarsi.ir/Mp3/1399/Bahram/Bahram%20-%20Beshno.mp3',
@@ -35,6 +35,6 @@ module.exports = (req, res) => {
       path: 'http://dlrapfarsi.ir/Mp3/1399/Bahram/Bahram%20-%20Beshno.mp3',
       artist: "BahramNori",
       type: 'JioSaavn',},
-              ]}
+              ]
   res.status(200).json({ date })
 };
